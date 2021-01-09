2ce398d77d31fd75fdbfb2d204c5981f9a53e0ef
Justinien Bouron <justinien.bouron@gmail.com>
2021-01-08 17:28:04 -0800

    Add bootstrap code to take care of the boot sequence and prepare the cpu before jumping to rust code.
    This code will setup a GDT, enable 32-bit Protected Mode, setup segment
    registers and a small stack in low memory.
    
    For now the cpu operates in 32-bit mode. 64-bit will be implemented
    later.
    
    The jump to the rust code is not yet implemented. More work on the rust
    build is necessary before being able to jump to it.

---
+++ bootstrap/asm_macros.h
@@ -0,0 +1,12 @@
+// This file contains macro(s) that are used in assembly file(s).
+
+// Placeholder for the 64-bit bit in the GDT entry. For now, only 32-bit is
+// supported.
+#define bits64 0
+
+// Create a GDT entry with the given base, limit and type.
+// The DPL will be set to ring 0 and granularity to 4KiB pages.
+#define GDT_ENTRY(base, limit, type)   \
+    (limit & 0xFFFF) | ((base & 0xFFFFFF) << 16) | (type << 40) | (1 << 44) | \
+    (0x0 << 45) | (1 << 47) | (((limit & 0xF0000) >> 16) << 48) | \
+    (bits64 << 53) | (1 << 54) | (1 << 55) | (((base & 0xFF) >> 24) << 56)
