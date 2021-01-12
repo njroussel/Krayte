7bbef43f51124ae3853cf3dc82217edbf21069b8
Justinien Bouron <justinien.bouron@gmail.com>
2021-01-11 18:09:56 -0800

    Bootstrap: Add mechanism to call 16-bit/real-mode functions from 32-bit protected mode. This new mechanism will do the transition to real-mode, execute the requested function, and return to 32-bit protected mode before returning from the caller.
    This commit adds more comments to the code as well, and separates the
    logging function from the bootstrap.S file.

--- bootstrap/asm_macros.h
+++ bootstrap/asm_macros.h
@@ -1,14 +1,31 @@
 // This file contains macro(s) that are used in assembly file(s).
 
-// Placeholder for the 64-bit bit in the GDT entry. For now, only 32-bit is
-// supported.
-#define bits64 0
-
-// Create a GDT entry with the given base, limit and type.
+// Create a GDT entry for a 16-bit segment with the given base, limit and type.
 // The DPL will be set to ring 0 and granularity to 4KiB pages.
-#define GDT_ENTRY(base, limit, type)   \
+#define GDT_ENTRY16(base, limit, type)   \
     (limit & 0xFFFF) | ((base & 0xFFFFFF) << 16) | (type << 40) | (1 << 44) | \
     (0x0 << 45) | (1 << 47) | (((limit & 0xF0000) >> 16) << 48) | \
-    (bits64 << 53) | (1 << 54) | (1 << 55) | (((base & 0xFF) >> 24) << 56)
+    (0 << 53) | (0 << 54) | (1 << 55) | (((base & 0xFF) >> 24) << 56)
+
+// Create a GDT entry for a 32-bit segment with the given base, limit and type.
+// The DPL will be set to ring 0 and granularity to 4KiB pages.
+#define GDT_ENTRY32(base, limit, type)   \
+    GDT_ENTRY16(base, limit, type) | (1 << 54)
 
 #define ARG(idx) (0x4 + idx << 1)
+
+// Define a 16-bit/real-mode function.
+// @param name: The name of the function.
+#define ASM_FUNC_DEF16(name)    ;\
+    .code16                     ;\
+    .global name                ;\
+    .type name, @function       ;\
+    name
+
+// Define a 16-bit/real-mode function.
+// @param name: The name of the function.
+#define ASM_FUNC_DEF32(name)    ;\
+    .code32                     ;\
+    .global name                ;\
+    .type name, @function       ;\
+    name
