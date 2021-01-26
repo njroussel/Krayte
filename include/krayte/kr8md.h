c3669c5c427d03b91bd05480df81f0145608fa4f
Nicolas Roussel <n.roussel13@gmail.com>
2021-01-27 00:11:18 +0100

    Create simple example for Pak construction

--- include/krayte/kr8md.h
+++ include/krayte/kr8md.h
@@ -4,22 +4,58 @@
 namespace kr8md
 {
 
-    template <typename T, size_t N>
-    struct pak
+    template <typename T, size_t W>
+    struct Pak
     {
-        T data[N];
+        union
+        {
+            T data[W];
+            __m256 m256;
+        };
+
+        Pak() {}
+
+        Pak(__m256 m256_) : m256(m256_) {}
+
+        Pak(T *arr)
+        {
+            for (size_t i = 0; i < W; i++)
+            {
+                data[i] = arr[i];
+            }
+        }
     };
 
-    template <typename T, size_t N>
-    pak<T, N> add(pak<T, N> a, pak<T, N> b)
+    template <typename T, size_t W>
+    Pak<T, W> add(Pak<T, W> a, Pak<T, W> b)
     {
-        pak<T, N> out;
-        for (size_t i = 0; i < N; i++)
+        Pak<T, W> out;
+        for (size_t i = 0; i < W; i++)
         {
             out.data[i] = a.data[i] + b.data[i];
         }
 
         return out;
+    }
+
+    template <int W>
+    using pfloatn = Pak<float, W>;
+    using pfloat4 = pfloatn<4>;
+
+    inline pfloat4 operator+(const pfloat4 &a, const pfloat4 &b)
+    {
+#ifdef __AVX2__
+        __m256 c_ = _mm256_add_ps(a.m256, b.m256);
+        return c_;
+#else
+        pfloat4 out;
+        for (size_t i = 0; i < 4; i++)
+        {
+            out.data[i] = a.data[i] + b.data[i];
+        }
+
+        return out;
+#endif
     }
 
 } // namespace kr8md
--- src/simd_test.cpp
+++ src/simd_test.cpp
@@ -4,23 +4,36 @@
 
 int main(void)
 {
-    using pfloat4 = kr8md::pak<float, 4>;
+    using pfloat4 = kr8md::Pak<float, 4>;
 
-    pfloat4 vec_a = {1.f,
-                     2.f,
-                     3.f,
-                     4.f};
+    float arr_a[4] = {1.f,
+                      2.f,
+                      3.f,
+                      4.f};
 
-    pfloat4 vec_b = {1.f,
-                     2.f,
-                     3.f,
-                     4.f};
+    float arr_b[4] = {2.f,
+                      3.f,
+                      4.f,
+                      5.f};
 
-    pfloat4 vec_c = kr8md::add(vec_a, vec_b);
+    pfloat4 vec_a(arr_a);
+    pfloat4 vec_b(arr_b);
 
+    std::cout << std::endl;
+    pfloat4 vec_c1 = kr8md::add(vec_a, vec_b);
+    pfloat4 vec_c2 = vec_a + vec_b;
+
+    std::cout << "c1" << std::endl;
     for (size_t i = 0; i < 4; i++)
     {
-        std::cout << vec_c.data[i] << std::endl;
+        std::cout << vec_c1.data[i] << std::endl;
+    }
+    std::cout << std::endl;
+
+    std::cout << "c2" << std::endl;
+    for (size_t i = 0; i < 4; i++)
+    {
+        std::cout << vec_c2.data[i] << std::endl;
     }
 
     return 0;
