33c78700d9fab09be795aa241a063ec7e687e285
Nicolas Roussel <n.roussel13@gmail.com>
2021-01-27 14:57:58 +0100

    Move std helpers for kr8md

--- include/krayte/kr8md.h
+++ include/krayte/kr8md.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <immintrin.h>
 
 namespace kr8md
--- src/simd_test.cpp
+++ src/simd_test.cpp
@@ -1,27 +1,7 @@
 #include <stdint.h>
 #include <iostream>
 #include <krayte/kr8md.h>
-
-namespace kr8md
-{
-    template <typename T, size_t W>
-    std::ostream &operator<<(std::ostream &os, const Pak<T, W> &pak)
-    {
-        os << "{";
-        for (size_t i = 0; i < W; i++)
-        {
-            os << pak.data[i];
-            if (i != W - 1)
-            {
-                os << ", ";
-            }
-        }
-        os << "}";
-
-        return os;
-    }
-
-} // namespace kr8md
+#include <krayte/kr8md_std_helpers.h>
 
 int main(void)
 {
