cf96e5d97d349203df65997fcb6d2c1150a11053
Justinien Bouron <justinien.bouron@gmail.com>
2021-02-09 16:08:02 -0800

    Bootstrap/Testing: Add testing framework for 64-bit mode tests.
    We reuse the test table. Each entry now has a byte indicating the mode
    it should be ran in. If the value is 32 then this is a 32-bit test, if
    the value is 64 then this is a 64-bit test.
    Testing is divided into two routines: run_tests32 (previously run_tests)
    runs 32-bit tests while run_tests64 runs 64-bit ones.

--- bootstrap/tests/test_macros.h
+++ bootstrap/tests/test_macros.h
@@ -5,5 +5,18 @@
     __test_table_str_ ## test_name:     ;\
     .asciz #test_name                   ;\
     .section .data.test_table           ;\
+    .byte 32                            ;\
+    .long __test_table_str_##test_name  ;\
+    .long test_name
+
+// Register a test in the test table to be executed by the run_tests routine.
+// 64-bit version.
+// @param test_name: The name of the test routine.
+#define REGISTER_TEST64(test_name)      ;\
+    .section .data                      ;\
+    __test_table_str_ ## test_name:     ;\
+    .asciz #test_name                   ;\
+    .section .data.test_table           ;\
+    .byte 64                            ;\
     .long __test_table_str_##test_name  ;\
     .long test_name
